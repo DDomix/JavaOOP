bb98757d98b7d1f3e08bf6289f02edbafe5584fd
Papp David <pappd377@gmail.com>
2022-09-15 11:13:03 +0200

    minden IS!!!!

--- src/petrik/hu/pontoop/Main.java
+++ src/petrik/hu/pontoop/Main.java
@@ -7,8 +7,33 @@ public class Main {
         Pont p1 = new Pont();
         Pont p2 = new Pont(0, 0);
         Pont p3 = new Pont(3, 4);
+        Pont p4 = new Pont(100);
+
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
+        System.out.println(p4);
+
+        Pont[] pontok = new Pont[10];
+        for (int i = 0; i < pontok.length; i++) {
+            pontok[i] = new Pont(100);
+        }
+        for (Pont p : pontok) {
+            System.out.println(p);
+        }
+
+        int legtavolabbipontindex = 0;
+        for (int i = 1; i < pontok.length; i++) {
+            if (pontok[legtavolabbipontindex].origotolmertavolsag() < pontok[i].origotolmertavolsag()) {
+                legtavolabbipontindex = i;
+            }
+        }
+        System.out.printf("legtávolabbi pont az origotol: %d pont melynek koordinatai: %s origotol mert tavolsaga:%.3f \n ",legtavolabbipontindex+1, pontok[legtavolabbipontindex],pontok[legtavolabbipontindex].origotolmertavolsag());
+
+        System.out.printf("pont1: %s, pont2: %s , két pont tavolsaga%.3f",p3,p4,p3.ketponttavolsaga(p4));
+        System.out.printf("A %s pont elhelyezkedese: %s \n",p3,p3.getSiknegyedszovege());
+        System.out.printf("A %s pont elhelyezkedese: %s \n",p2,p2.getSiknegyedszovege());
+        System.out.printf("A %s pont elhelyezkedese: %s \n",p4,p4.getSiknegyedszovege());
     }
 }
+
--- src/petrik/hu/pontoop/Pont/pont.java
+++ src/petrik/hu/pontoop/Pont/pont.java
@@ -14,11 +14,73 @@ public class Pont {
         this.y = y;
     }
 
+    public Pont(int n) {
+        this.x = koordinatageneralasa(n);
+        this.y = koordinatageneralasa(n);
+    }
+
+    private int koordinatageneralasa(int n) {
+        return (int) (Math.random() * (2 * n + 1)) - n;
+    }
+
+    public double origotolmertavolsag() {
+        return Math.sqrt((Math.pow(this.x, 2) + Math.pow(this.y, 2)));
+    }
+
+    public double ketponttavolsaga(Pont masikpont) {
+        return Math.sqrt(Math.pow(this.x - masikpont.x, 2) + Math.pow(this.y - masikpont.y, 2));
+    }
+
+    public int getSiknegyed() {
+        int siknegyed = 0;
+        if (this.x < 0 && this.y > 0) {
+            siknegyed = 1;
+        } else if (this.x < 0 && this.y < 0) {
+            siknegyed = 2;
+        } else if (this.x > 0 && this.y < 0) {
+            siknegyed = 3;
+        } else if (this.x > 0 && this.y > 0) {
+            siknegyed = 4;
+        } else if (this.x == 0 && this.y != 0){
+            siknegyed=5;
+        }else if (this.x!=0&&this.y==0){
+            siknegyed=6;
+        }
+        return siknegyed;
+    }
+
+    public  String getSiknegyedszovege(){
+        int siknegyed=this.getSiknegyed();
+        String siknegyedszovege="";
+        switch (siknegyed){
+            case 0:
+                siknegyedszovege="origo";
+                break;
+            case 1:
+                siknegyedszovege="bal felso";
+                break;
+            case 2:
+                siknegyedszovege="bal also";
+                break;
+            case 3:
+                siknegyedszovege="jobb also";
+                break;
+            case 4:
+                siknegyedszovege="jobb felso";
+                break;
+            case 5:
+                siknegyedszovege="függőleges tengely";
+                break;
+            case 6:
+                siknegyedszovege="vízszintes tengely";
+                break;
+        }
+        return siknegyedszovege;
+    }
+
     @Override
     public String toString() {
-        return "Pont{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+        //return "(" + x + "," + y + ")";
+        return String.format("(%d, %d)", this.x, this.y);
     }
 }
