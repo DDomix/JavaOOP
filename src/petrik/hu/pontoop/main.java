146b48343ae69e3bc3fe3c48c6572a1f8af2595a
Papp David <pappd377@gmail.com>
2022-09-15 08:46:30 +0200

    pont osztaly konstruktorok

--- src/petrik/hu/pontoop/main.java
+++ src/petrik/hu/pontoop/main.java
@@ -1,4 +1,7 @@
 package petrik.hu.pontoop;
 
 public class main {
+    public static void main(String[] args){
+
+    }
 }
---
+++ src/petrik/hu/pontoop/pont/pont.java
@@ -0,0 +1,17 @@
+package petrik.hu.pontoop.pont;
+
+public class pont {
+    private int x;
+    private int y;
+
+    public pont() {
+        x = 0;
+        y = 0;
+    }
+
+    public pont(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
